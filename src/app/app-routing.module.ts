c002d9cad6c6de7987a969d8af1fc26388ffd8d9
Ignacio <imoon521@gmail.com>
2019-09-17 16:53:57 -0600

    Create index and events modules & components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadChildren: () => import('./index/index.module').then((m) => m.IndexModule)
+	},
+	{
+		path: 'events',
+		pathMatch: 'full',
+		loadChildren: () => import('./events/events.module').then((m) => m.EventsModule)
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
