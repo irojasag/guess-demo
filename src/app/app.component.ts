0e79c139855d95039069f40d1fd86d6d5d6d4d76
Ignacio <imoon521@gmail.com>
2019-09-17 20:24:13 -0600

    Add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'ng-costa-rica';
+	title = 'ng-costa-rica';
+	constructor(private router: Router) {
+		this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+			gtag('config', 'UA-147425019-1', {
+				page_path: event.urlAfterRedirects
+			});
+		});
+	}
 }
